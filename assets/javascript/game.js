516995f383eef354d53755ad83e2772ca38cc441
sorines <sarah.orines@gmail.com>
2018-03-21 22:48:26 -0700

    the score tracker works however the wins does not

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,6 @@
 var win = 0;
 var loss = 0;
 var crystal = ["#crystalBlue", "#crystalRed", "#crystalGreen", "#crystalPurple"];
-var counter = 0;
 
 //this is where the random number to beat will be 
 
@@ -18,34 +17,37 @@ $(document).ready(function () {
     console.log("#numberToBeat");
 });
 
-
+var totalScore= 0;
 //this next spot will give value to the cystals...maybe. 
 //trying to use the image to click and create a value
 $(".crystal").on("click", crystal, function () {
 
-    var totalScore = "";
+
 
     for (var i = 0; i < 1; i++) {
 
         var crystal = Math.floor(Math.random() * 12) + 1;
-        totalScore = crystal + totalScore;
-        counter++;
+         totalScore += crystal ;
+    
         //this is going to print out the total score 
 
-
+        
         console.log("#totalScore");
     }
 
     $("#totalScore").text(totalScore);
 //prints out the loss and wins
+    
     if (totalScore === numberToBeat) {
         win++;
-        $(".win").text("Win: " + win);
+       $(".win").text("Win: " + win);
+    
     }
+    
     else {
         loss++;
         $(".loss").text("Loss: " + loss);
     }
-
-
+    
+    
 });
